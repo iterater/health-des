031c63250c8eac57b767e9b8b219220049b7b6d7
Sergey V. Kovalchuk <sergey.v.kovalchuk@gmail.com>
2017-01-30 18:36:41 +0300

    Trying to simulate LoS

--- experiment_01_acs.py
+++ experiment_01_acs.py
@@ -3,6 +3,8 @@ import numpy as np
 import state_info
 import dept_des
 import datetime
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 # reading paths clusters
 clusters = pd.read_csv('data\\acs\\clusters_size.csv', delimiter=';')
@@ -22,11 +24,19 @@ background_surgery_duration_gen = state_info.RvFromData(np.loadtxt('data\\total_
 
 # simulation run
 total_log = []
-background_scale = 1.0
+background_scale = 0.7
 surgeries_number = 2
-simulation_time = 30*24*60
+simulation_time = 60*24*60
 sim_res = dept_des.simulate_patients_flow(acs_patients_gen, acs_event_gen, surgeries_number, background_surgery_gen,
                                           background_surgery_duration_gen, background_scale, simulation_time)
+# counting LoS
+# sim_res.to_csv('logs\\sim-res-' + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.csv')
+los_gr = sim_res[(sim_res.ID >= 0) | (sim_res.STATE.str.contains('E'))].groupby(['ID', 'STATE'])['TIME']
+los = np.array((los_gr.max() - los_gr.min()).sum(level=0), dtype=float)
+sns.distplot(los / 60.0)
+plt.savefig('pics\\los-E-' + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.png')
+plt.close()
+# saving stats
 sim_stats = dept_des.get_queue_statistics(sim_res)
 sim_stats['SCALE'] = background_scale
 sim_stats['N_SURG'] = surgeries_number
@@ -34,6 +44,6 @@ print(sim_stats)
 total_log.append(sim_stats)
 
 # writing total log
-total_log_df = pd.DataFrame(total_log, columns=total_log[0].keys())
-total_log_df.to_csv('logs\\queue-stats-' + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.csv')
+# total_log_df = pd.DataFrame(total_log, columns=total_log[0].keys())
+# total_log_df.to_csv('logs\\queue-stats-' + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S') + '.csv')
 
